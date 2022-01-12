dca36af796657f88ecf5ac61742ac26745219085
liuzhengbing <liuzhengbing@bytedance.com>
2022-01-12 15:57:25 +0800

    fix: 111

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import './global.less';
 
-console.log('环境变量:111', ICON_URL, process.env.NODE_ENV);
+console.log('环境变量:', ICON_URL, process.env.NODE_ENV);
 ReactDOM.render(<App />, document.getElementById('root'));

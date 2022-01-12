722d9c4a1322d28329127accfc5587c8d8a25cca
liuzhengbing <liuzhengbing@bytedance.com>
2022-01-12 16:07:44 +0800

    fix: 111

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export const App: React.FC = () => {
   const [title, setTitle] = useState('hello world');
-  console.log(title);
+  console.log('title', title);
   return (
     <>
       <h1 onClick={() => setTitle('123')}>{title}</h1>
